7cf611332a14d512046bf517d56767de852e1a71
aljack129 <abbyjacks22@gmail.com>
2026-09-14 10:03:56 -0600

    pieceMoves and helper functions

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -52,7 +53,9 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> moves = new ArrayList();
+        checkDirections(myPosition, myPosition, board, moves);
+        return moves;
     }
 
     private final int[][] bishop_directions = {{-1,1}, {-1,-1}, {1,1}, {1,-1}};
@@ -68,4 +71,103 @@ public class ChessPiece {
             return true;
         }
     }
+
+    private void checkDirections(ChessPosition start, ChessPosition spot, ChessBoard board, Collection<ChessMove> moves) {
+        if (board.validSpot(spot)) {
+            int [][] directions = {{}};
+            if(spot == start || board.getPiece(spot) == null){
+                moves.add(new ChessMove(start, spot, type));
+
+                if (type == ChessPiece.PieceType.BISHOP) {
+                    directions = bishop_directions;
+                }
+                else if(type == ChessPiece.PieceType.ROOK){
+                    directions = rook_directions;
+                }
+                else if(type == ChessPiece.PieceType.QUEEN){
+                    directions = queen_king_directions;
+                }
+                else if (type == ChessPiece.PieceType.KING){
+                    directions = queen_king_directions;
+                    for (int[] dir : directions){
+                        ChessPosition newSpot = new ChessPosition(spot.getRow() + dir[0], spot.getColumn() + dir[1]);
+                        if (board.getPiece(newSpot) != null) {
+                            if (canCapture(board.getPiece(newSpot))){
+                                moves.add(new ChessMove(start, newSpot, type));
+                            }
+                        }
+                        else{
+                            moves.add(new ChessMove(start, newSpot, type));
+                        }
+                    }
+                    return;
+                }
+                else if(type == ChessPiece.PieceType.KNIGHT){
+                    directions = knight_directions;
+                    for (int[] dir : directions){
+                        ChessPosition newSpot = new ChessPosition(spot.getRow() + dir[0], spot.getColumn() + dir[1]);
+                        if (board.getPiece(newSpot) != null) {
+                            if (canCapture(board.getPiece(newSpot))){
+                                moves.add(new ChessMove(start, newSpot, type));
+                            }
+                        }
+                        else{
+                            moves.add(new ChessMove(start, newSpot, type));
+                        }
+                    }
+                    return;
+                }
+                else if(type == ChessPiece.PieceType.PAWN){
+                    if(color == ChessGame.TeamColor.BLACK){
+                        ChessPosition newSpot = new ChessPosition(spot.getRow() + 0, spot.getColumn() -1);
+                        if(board.getPiece(newSpot) == null){
+                            moves.add(new ChessMove(start, newSpot, type));
+                        }
+                        int[][] pawn_directions = {{-1,-1}, {1,-1}};
+                        for (int[] dir : pawn_directions){
+                            newSpot = new ChessPosition(spot.getRow() + dir[0], spot.getColumn() + dir[1]);
+                            if (board.getPiece(newSpot) != null) {
+                                if (canCapture(board.getPiece(newSpot))){
+                                    moves.add(new ChessMove(start, newSpot, type));
+                                }
+                            }
+                        }
+                        return;
+                    }
+                    else{
+                        ChessPosition newSpot = new ChessPosition(spot.getRow() + 0, spot.getColumn() + 1);
+                        if(board.getPiece(newSpot) == null){
+                            moves.add(new ChessMove(start, newSpot, type));
+                        }
+                        int[][] pawn_directions = {{-1,1}, {1,1}};
+                        for (int[] dir : pawn_directions){
+                            newSpot = new ChessPosition(spot.getRow() + dir[0], spot.getColumn() + dir[1]);
+                            if (board.getPiece(newSpot) != null) {
+                                if (canCapture(board.getPiece(newSpot))){
+                                    moves.add(new ChessMove(start, newSpot, type));
+                                }
+                            }
+                        }
+                        return;
+                    }
+                }
+
+                for (int[] dir : directions){
+                    ChessPosition newSpot = new ChessPosition(spot.getRow() + dir[0], spot.getColumn() + dir[1]);
+                    checkDirections(start, newSpot, board, moves);
+                }
+                return;
+            }
+            else if (board.getPiece(spot) != null) {
+                if (canCapture(board.getPiece(spot))){
+                    moves.add(new ChessMove(start, spot, type));
+                }
+            }
+
+        }
+        else{
+            return;
+        }
+    }
+
 }
